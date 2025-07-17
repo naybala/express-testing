8f590e9b400f9424130e068b77b2538babafd048
Nay Ba La <naybalaismylife.nbl@gmail.com>
2025-07-17 11:37:37 +0630

    modify : linn linn branch

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,35 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+/**
+ * @docs https://www.prisma.io/docs/orm/prisma-client/observability-and-logging/logging
+ */
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  if (process.env.LISTEN_DB === "true") {
+    console.log("Query: " + e.query);
+    console.log("Params: " + e.params);
+    console.log("Duration: " + e.duration + "ms");
+  }
+});
 
 export default prisma;
--- modules/Mobile/products/services/index.ts
+++ modules/Mobile/products/services/index.ts
@@ -2,6 +2,6 @@ import { productRepository } from "@domain/product/product.repository";
 import { indexProductResource,IndexProductInterface } from "../resources";
 
 export const get = async (): Promise<IndexProductInterface[]> => {
-    const products = await productRepository.get();
+    const products = await productRepository.with("category:id,name").get();
     return products.map(indexProductResource);
 }
